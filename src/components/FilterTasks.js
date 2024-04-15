07194fa2b6cd59ad89bcdf0adbe03af9aa93534c
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-04-15 12:09:11 +0530

    added css for filter

--- src/components/FilterTasks.js
+++ src/components/FilterTasks.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import { filterTasks, updateOptions } from "../redux/TaskTrackerSlice";
 import { useDispatch } from 'react-redux';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export function FilterTasks(props){
     const [showDropdown, setShowDropdown] = useState(false);
     const dispatch = useDispatch()
+    const dropDownRef = useRef(null)
 
     const toggleDropdown = () => {
         setShowDropdown(!showDropdown);
@@ -24,9 +25,17 @@ export function FilterTasks(props){
         dispatch(filterTasks({options: updatedOptions}))
     }
 
+    const closeDropDown = (e) => {
+        if(showDropdown && !dropDownRef.current?.contains(e.target)){
+            setShowDropdown(false)
+        }
+    }
+
+    document.addEventListener('mousedown', closeDropDown)
+
     return (
         <>
-            <div className="filter-btn">
+            <div className="filter-btn" ref={dropDownRef}>
                 <a>
                     <i className="bi bi-filter fs-2" onClick={toggleDropdown}></i>
                 </a>
@@ -36,6 +45,7 @@ export function FilterTasks(props){
                         <input
                             type="checkbox"
                             id="incomplete"
+                            className="me-3"
                             checked={props.selectedOptions.includes("Incomplete")}
                             onChange={() => handleOptionChange("Incomplete")}
                         />
@@ -45,6 +55,7 @@ export function FilterTasks(props){
                         <input
                             type="checkbox"
                             id="inprogress"
+                            className="me-3"
                             checked={props.selectedOptions.includes("Inprogress")}
                             onChange={() => handleOptionChange("Inprogress")}
                         />
@@ -54,6 +65,7 @@ export function FilterTasks(props){
                         <input
                             type="checkbox"
                             id="completed"
+                            className="me-3"
                             checked={props.selectedOptions.includes("Completed")}
                             onChange={() => handleOptionChange("Completed")}
                         />
